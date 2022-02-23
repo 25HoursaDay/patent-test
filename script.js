7e3c6aa42dba05ceed48219b282acebc29d8f4c0
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-02-23 10:12:14 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+var lower = 10000000;
+var upper = 99999999;
+window.location.href = "https://tsdr.uspto.gov/#caseNumber=" + (Math.floor(Math.random()*(upper-lower))+lower) + "&caseSearchType=US_APPLICATION&caseType=DEFAULT&searchType=statusSearch"
