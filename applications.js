55768ac1e9c2139c4b2844471a1ddf7901316c98
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-18 14:05:55 -0500

    Update applications.js

--- applications.js
+++ applications.js
@@ -1,3 +1,5 @@
 var win = window.open("https://patft.uspto.gov/netahtml/PTO/search-bool.html");
 win.document.getElementById("trm1").value = (Math.floor(Math.random()*("99999999"-"1000000"))+"1000000");
 win.document.getElementById("fld1").value = "PN";
+win.document.getElementById("yrs").value = "PALL";
+win.document.getElementByName("search_pat").submit();
