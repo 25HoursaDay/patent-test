a289b7e718ee1b4d3766984ee7e89e0ae1e79af9
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-18 12:17:01 -0500

    Update applications.js

--- applications.js
+++ applications.js
@@ -1,12 +1,3 @@
-var url = "https://patentcenter.uspto.gov/#!/applications/"+(Math.floor(Math.random()*("99999999"-"10000000"))+"10000000");
-var win;
-win = window.open("https://www.uspto.gov/");
-win.document.body.style.margin = "0";
-win.document.body.style.height = "100vh";
-var iframe = win.document.createElement("iframe");
-iframe.style.border = "none";
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.margin = "0";
-iframe.src = url;
-win.document.body.appendChild(iframe);
+var win = window.open("https://patft.uspto.gov/netahtml/PTO/search-bool.html");
+win.document.getElementById("trm1").value = (Math.floor(Math.random()*("99999999"-"1000000"))+"1000000");
+win.document.getElementById("fld1").value = "PN";
