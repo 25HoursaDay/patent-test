f4386562bfb1d99ba6304427a9088180f31bc8fd
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-18 08:41:16 -0500

    Update applications.js

--- applications.js
+++ applications.js
@@ -1,3 +1,5 @@
+var bruh = document.createElement("patent");
+
 var lower = 10000000;
 var upper = 99999999;
 location.replace('https://patentcenter.uspto.gov/#!/applications/'+(Math.floor(Math.random()*(upper-lower))+lower));
