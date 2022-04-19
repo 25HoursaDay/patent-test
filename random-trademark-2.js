2ab220101ab73d0c5ccb1e0fe5bd3bfa0bead5ba
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-19 09:39:16 -0500

    Delete random-trademark-2.js

--- random-trademark-2.js
+++
@@ -1,3 +0,0 @@
-var lower = 1000000;
-var upper = 9999999;
-window.open("https://assignment.uspto.gov/trademark/index.html#/trademark/search/resultAbstract?id="+(Math.floor(Math.random()*(upper-lower))+lower)+"&type=serialNum");
