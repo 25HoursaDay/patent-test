2c9d6e68529686f1fdbca38a4aad1f3b72ac97cf
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-19 08:55:23 -0500

    Update random-trademark-2.js

--- random-trademark-2.js
+++ random-trademark-2.js
@@ -1,3 +1,3 @@
-var lower = 10000000;
-var upper = 99999999;
-window.location.href="https://assignment.uspto.gov/trademark/index.html#/trademark/search/resultAbstract?id="+(Math.floor(Math.random()*(upper-lower))+lower)+"&type=serialNum"
+var lower = 1000000;
+var upper = 9999999;
+window.open("https://assignment.uspto.gov/trademark/index.html#/trademark/search/resultAbstract?id="+(Math.floor(Math.random()*(upper-lower))+lower)+"&type=serialNum");
