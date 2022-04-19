c62045769ee9062da45ac4323dc258cd6f7ae5fd
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-19 08:52:55 -0500

    Update patent-search.js

--- patent-search.js
+++ patent-search.js
@@ -1,4 +1,3 @@
-
 var lower = 1000000;
 var upper = 9999999;
 window.open("https://assignment.uspto.gov/patent/index.html#/patent/search/resultAbstract?id="+(Math.floor(Math.random()*(upper-lower))+lower)+"&type=patNum");
