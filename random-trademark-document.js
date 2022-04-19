3d9c8f276d469789cce55ee69248e0ac7f113217
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-04-19 09:40:04 -0500

    Delete random-trademark-document.js

--- random-trademark-document.js
+++
@@ -1,3 +0,0 @@
-var lower = 10000000;
-var upper = 99999999;
-window.location.href="https://tsdr.uspto.gov/#caseNumber="+(Math.floor(Math.random()*(upper-lower))+lower)+"&caseSearchType=US_APPLICATION&caseType=DEFAULT&searchType=documentSearch"
